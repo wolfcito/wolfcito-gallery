157ddf08ac3fd6cc1a6eda02569db9eb4dbec07e
Luis Fernando <guffenix@gmail.com>
2024-05-25 12:52:50 -0500

    feat: inluding resources for new gallery

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,29 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
-          <meta
-            property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <link rel="icon" href="/favicon.png" />
+          <meta name="description" content="Wolfcito's gallery" />
+          <meta property="og:site_name" content="wolfcito.xyz" />
+          <meta property="og:description" content="Wolfcito's gallery" />
+          <meta property="og:title" content="Momentos increíbles de Wolfcito" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
           <meta
-            name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            name="twitter:title"
+            content="Momentos increíbles de Wolfcito"
           />
+          <meta name="twitter:description" content="Wolfcito's gallery" />
         </Head>
         <body className="bg-black antialiased">
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
--- utils/types.ts
+++ utils/types.ts
@@ -1,19 +1,19 @@
 /* eslint-disable no-unused-vars */
 export interface ImageProps {
-  id: number;
-  height: string;
-  width: string;
-  public_id: string;
-  format: string;
-  blurDataUrl?: string;
+  id: number
+  height: string
+  width: string
+  public_id: string
+  format: string
+  blurDataUrl?: string
 }
 
 export interface SharedModalProps {
-  index: number;
-  images?: ImageProps[];
-  currentPhoto?: ImageProps;
-  changePhotoId: (newVal: number) => void;
-  closeModal: () => void;
-  navigation: boolean;
-  direction?: number;
+  readonly index: number
+  readonly images?: ImageProps[]
+  readonly currentPhoto?: ImageProps
+  readonly changePhotoId: (newVal: number) => void
+  readonly closeModal: () => void
+  readonly navigation: boolean
+  readonly direction?: number
 }
